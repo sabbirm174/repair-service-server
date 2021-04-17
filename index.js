ccbdb8d64f742c3fdffaf15915b34804d82029cf
Sabbir Alam <sabbirm174@gmail.com>
2021-04-17 23:58:04 +0600

    first commit

--- index.js
+++ index.js
@@ -1,84 +1,74 @@
 const express = require('express')
 const app = express();
-const cors = require('cors')
-const bodyParser =require('body-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-const { ObjectID } = require('mongodb');
-require('dotenv').config()
-
-const port = process.env.PORT || 5055;
-
-//middleware
-app.use(cors());
-// middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzmkl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const port = process.env.PORT || 2000;
+
+//middle ware
+app.use(cors());
+app.use(bodyParser.json())
+
+//method
+app.get('/',(req,res)=>{
+  res.send("hello world")
+})
+
+//database connect
+
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const productsCollection = client.db("EasyShop").collection("Products");
-  const addProductsCollection = client.db("EasyShop").collection("addProduct");
-  
-  //post into database
-  app.post('/addEvent',(req,res)=>{
+  const serviceCollection = client.db("Courier").collection("Service");
+  const bookingCollection = client.db("Courier").collection("booking");
+  const adminCollection = client.db("Courier").collection("admin");
+  // perform actions on the collection object
+  //client.close();
+  app.post('/addbook',(req,res)=>{
     const newEvent = req.body;
-    productsCollection.insertOne(newEvent)
+    console.log('addign new evwnt', newEvent);
+    bookingCollection.insertOne(newEvent)
     .then(result=>{
-      console.log('inserted count', result.insertedCount)
-      if(result.insertedCount> 0){
-        res.send(newEvent);
-      }
-      
-    })
-  })
-  
-  //post single product into database
-  app.post('/addProduct',(req,res)=>{
-    const newEvent = req.body;
-    addProductsCollection.insertOne(newEvent)
-    .then(result=>{
-      console.log('inserted count', result.insertedCount)
-      if(result.insertedCount> 0){
-        res.send(newEvent);
-      }
+      console.log(result.insertedCount)
+      res.send(result.insertedCount>0)
     })
   })
 
-  //single product load
-  app.get("/edit/:id",(req,res)=>{
-    const id = req.params.id;
-    productsCollection.find({_id: ObjectID(id)})
+  //add services
+  app.post('/addservices',(req,res)=>{
+    const newService = req.body;
+    serviceCollection.insertOne(newService)
+    .then(result=>{
+      console.log(result.insertedCount)
+      res.send(result.insertedCount>0)
+    })
+  })
+
+  //add admin
+  app.post('/addadmin',(req,res)=>{
+    const newEvent = req.body;
+    console.log('addign new admin', newEvent);
+    adminCollection.insertOne(newEvent)
+    .then(result=>{
+      console.log(result.insertedCount)
+      res.send(result.insertedCount>0)
+    })
+  })
+  
+  // get all serivice
+  app.get('/allservice',(req,res)=>{
+    serviceCollection.find({})
     .toArray((err,document)=>{
       res.send(document)
     })
   })
-  //single product load
-  app.delete("/delete/:id",(req,res)=>{
-    const id = req.params.id;
-    productsCollection.deleteOne({_id: ObjectID(id)})
-
-  })
-
-  // get from database
-  app.get("/allProducts",(req,res)=>{
-    productsCollection.find()
-    .toArray((err,items)=>{
-      res.send(items);
-    })
-  })
-
-  // get from database
-  app.get("/loadProduct",(req,res)=>{
-    const email = req.query.email;
-    console.log(email);
-    addProductsCollection.find({email:email})
-    .toArray((err,items)=>{
-      res.send(items);
-    })
-  })
-  
+  console.log('database connected successfully')
 });
+
+
 
 
 
